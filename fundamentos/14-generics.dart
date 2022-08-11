99a14c79bf6e6920ef1203f3cf062b999b7da616
Giuseppe Rosa <giusepper11@gmail.com>
2022-08-11 16:09:21 -0300

    14 - generics

---
+++ fundamentos/14-generics.dart
@@ -0,0 +1,32 @@
+main() {
+  List<String> listaCoisas1 = [
+    'banana',
+    'maca',
+    // true,
+    // 133,
+    // 4.56,
+    // [1, 2, 3]
+  ];
+
+
+  var listaCoisas2 = [
+    'banana',
+    'maca',
+    true,
+    133,
+    4.56,
+    [1, 2, 3]
+  ];
+
+  Map<String, double> salarios = {
+    'gerente': 19345.57,
+    'vendedor': 16098.56,
+    'estagiario': 600.00
+  };
+
+  var salarios2 = {
+    'gerente': 19345.57,
+    'vendedor': 16098.56,
+    'estagiario': '600.00'
+  };
+}
